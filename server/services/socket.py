eadc45f9d57796033db85fba8eb3d45d2750c8e1
asterki <asterki.dev@proton.me>
2024-06-10 22:27:15 -0600

    Feat: Can now control a servo through web interface

--- server/services/socket.py
+++ server/services/socket.py
@@ -1,20 +1,22 @@
 import flask_socketio
 import pyfirmata
+import threading
+import time
 
-
-board = pyfirmata.Arduino("/dev/ttyACM0")
+board = pyfirmata.Arduino("/dev/ttyACM1")
 
 board_thread = pyfirmata.util.Iterator(board)
 board_thread.start()
+
 
 class SocketService:
     instance = None
 
     def __init__(self, app):  # The app is passed as an argument, flask app instance
         self.app = app
+        self.servo_pin = board.get_pin("d:9:s")
+        
         self.socketio = flask_socketio.SocketIO(self.app, cors_allowed_origins="*")
-        
-        
 
         # Register events
         self.register_server_events()
@@ -38,21 +40,20 @@ class SocketService:
             print("Client disconnected")
 
     def register_client_events(self):
-        @self.socketio.on("test1")
-        def test1(data):
-            print("Test event received")
-            if data["value"] == "on":
-                board.digital[12].write(1)
-            else:
-                board.digital[12].write(0)
+        @self.socketio.on("relay")
+        def relays(data):
+            relays = [2, 3, 4, 5]
+            board.digital[relays[data["relay"]-1]].write(data["value"])
             
-        @self.socketio.on("test2")
-        def test2(data):
-            print("Test event received")
-            if data["value"] == "on":
-                board.digital[11].write(1)
-            else:
-                board.digital[11].write(0)
+        @self.socketio.on("angle")
+        def angle(data):
+            self.servo_pin.write(0)
+            time.sleep(2)
+            
+            for i in range(0, 90, 10):
+                print(i)
+                self.servo_pin.write(i)
+                time.sleep(0.5)
 
     def start_socket_server(self):
         self.socketio.run(self.app)
