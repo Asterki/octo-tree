81e1e47e0d7054157b4b6143a07c3625f18e5340
Fernando Rivera <asterki.dev@proton.me>
2024-06-13 21:23:53 -0600

    Update socket.py

--- server/services/socket.py
+++ server/services/socket.py
@@ -1,17 +1,36 @@
 import flask_socketio
 import pyfirmata
 import time
+import threading
+from datetime import datetime
+import time
 
-board = pyfirmata.Arduino("/dev/ttyACM1")
+from services.database import DatabaseService
+
+board = pyfirmata.Arduino("COM9")
 
 board_thread = pyfirmata.util.Iterator(board)
 board_thread.start()
 
+global_date = 0
+def cosa():
+    while True:
+        time.sleep(2)
+        print(global_date, time.time())
+
+        if ((time.time() * 1000) > global_date) and global_date != 0:
+            while True:
+                board.digital[2].write(1)
+                time.sleep(2)
+                board.digital[2].write(0)
+                time.sleep(2)
+
+threading.Thread(target=cosa).start()
 
 class SocketService:
     instance = None
 
-    def __init__(self, app):  # The app is passed as an argument, flask app instance
+    def _init_(self, app):  # The app is passed as an argument, flask app instance
         self.app = app
         self.servo_pin = board.get_pin("d:9:s")
         
@@ -47,12 +66,18 @@ class SocketService:
         @self.socketio.on("angle")
         def angle(data):
             self.servo_pin.write(0)
-            time.sleep(2)
+            time.sleep(0.1)
             
-            for i in range(0, 90, 10):
+            for i in range(0,15, 30, 45, 60,75,90):
                 print(i)
                 self.servo_pin.write(i)
                 time.sleep(0.5)
+
+        @self.socketio.on("date")
+        def datething(data):
+            global_date = data
+            print(global_date)
+            
 
     def start_socket_server(self):
         self.socketio.run(self.app)
