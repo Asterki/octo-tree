6677feb4dcccce3e9ce7e5f86b87aa37fd1ad75e
asterki <asterki.dev@proton.me>
2024-06-15 10:16:46 -0600

    Feat: Board service

--- server/services/board.py
+++ server/services/board.py
@@ -8,11 +8,15 @@ class BoardService:
     instance = None
     
     def __init__(self):
+        # Connect to the board
         self.board = pyfirmata.Arduino('/dev/ttyACM0')
         self.it = pyfirmata.util.Iterator(self.board)
+        
+        # Start the iterator
         self.board.iterate()
         self.it.start()
 
+        # Define de Ax pins
         self.sensors = [
             self.board.get_pin('a:1:i'),
             self.board.get_pin('a:2:i'),
@@ -21,6 +25,15 @@ class BoardService:
             self.board.get_pin('a:5:i'),
         ]
         
+        self.servos = [
+            self.board.get_pin('d:6:s'),
+            self.board.get_pin('d:7:s'),
+            self.board.get_pin('d:8:s'),
+            self.board.get_pin('d:9:s'),
+            self.board.get_pin('d:10:s'),
+        ]
+        
+        # Singleton
         if BoardService.instance is None:
             BoardService.instance = self
         
@@ -33,10 +46,19 @@ class BoardService:
         return self.board
 
     def write_pin(self, pin, value):
-        self.board.digital[pin].write(value)
+        self.board.digital[pin].write(value)  # Write the value to the pin
         
     def read_sensor(self, sensor):
-        self.sensors[sensor].enable_reporting()
-        return self.sensors[sensor].read()
+        self.sensors[sensor].enable_reporting()  # Enable the reporting
+        return self.sensors[sensor].read()  # Read the value
 
+    def move_servo(self, servo, angle, speed):
+        # Move the servo
+        def move():
+            for i in range(0, angle, speed):
+                self.servos[servo].write(i) # Write the angle to the servo
+                time.sleep(0.1)
+            
+        # Start the thread to avoid blocking the main thread
+        threading.Thread(target=move).start()
         
