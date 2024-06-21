e6b3ea12d35ccb3d02219ac44ac419efcc66fe9b
asterki <asterki.dev@proton.me>
2024-06-20 21:25:41 -0600

    Fix: Removed innecessary REST field

--- server/routes/access.py
+++ server/routes/access.py
@@ -9,7 +9,6 @@ from services.sessions import SessionsManager
 access_router = Blueprint('access_router', __name__ , url_prefix='/api/access')
 
 class LoginData(BaseModel):
-    username: str = Field(max_length=16, min_length=1)
     password: str = Field(max_length=16, min_length=1)
 @access_router.route('/login', methods=['POST'])
 def login():
