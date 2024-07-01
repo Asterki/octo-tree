aab590f7e7aefa4445f5da776d258170c8e4a4d2
asterki <asterki.dev@proton.me>
2024-07-01 06:12:09 -0600

    Fix: Landing page not properly set at root route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,7 +11,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
     {
-        path: "/index",
+        path: "/",
         element: <IndexPage />,
     },
     {
