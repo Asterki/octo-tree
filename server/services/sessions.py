8a67c8d4bfcb50561cac5996418749485a2d92de
Fernando Rivera <asterki.dev@proton.me>
2024-07-06 11:58:46 -0600

    Session system fix (#17)
    * Fix: Fixed session system by switching from os.env to dotenv
    
    * Fix: Fixed status messages popping when getting a response from the server

--- server/services/sessions.py
+++ server/services/sessions.py
@@ -1,10 +1,11 @@
 import jwt
 import uuid
-import os
 import datetime
+from dotenv import dotenv_values
 
 from services.database import DatabaseService
 
+config = dotenv_values(".env")
 
 class SessionsManager:
     _instance = None
@@ -25,7 +26,7 @@ class SessionsManager:
         token = str(uuid.uuid4())
         expires = datetime.datetime.now() + datetime.timedelta(days=1)
         date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        secret = os.getenv("SESSION_SECRET")
+        secret = config["SESSION_SECRET"]
 
         try:
             # Insert the session into the database
@@ -37,6 +38,9 @@ class SessionsManager:
                     "expires_at": expires.strftime("%Y-%m-%d %H:%M:%S"),
                 },
             )
+            
+            print(secret, token)
+            
             # Return the token
             return jwt.encode(
                 {"token": token, "expires": int(expires.timestamp() * 1000)}, secret, algorithm="HS256"
@@ -46,7 +50,7 @@ class SessionsManager:
         return False
 
     def verify_session(self, token):
-        secret = os.getenv("SESSION_SECRET")
+        secret = config["SESSION_SECRET"]
         try:
             decoded = jwt.decode(token, secret, algorithms=["HS256"])
             session = DatabaseService.get_instance().get(
@@ -67,7 +71,7 @@ class SessionsManager:
         return False
 
     def delete_session(self, token):
-        secret = os.getenv("SESSION_SECRET")
+        secret = config["SESSION_SECRET"]
 
         try:
             decoded = jwt.decode(token, secret, algorithms=["HS256"])
