1914c2b6445e5bf5819fa67c1a8fa5730a68b523
asterki <asterki.dev@proton.me>
2024-08-07 21:22:38 -0600

    Feat: User can now access a sample page

--- hardware/esp32/index.ino
+++ hardware/esp32/index.ino
@@ -1,34 +1,36 @@
 #include <WiFi.h>
-
-const char* ssid = "";             // Change this to your WiFi SSID
-const char* password = "";   // Change this to your WiFi password
+#include <NetworkClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
 
 const char* host = "192.168.0.15";         // This should not be changed
 const int httpPort = 5000;                 // This should not be changed
 
+NetworkServer server(80); // Server used to set the credentials for a WiFi network
+
 void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(115200);
     while (!Serial) {
         delay(100);
     }
-
-    // Connect to WiFi network
-    Serial.println();
-    Serial.println("******************************************************");
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+    
+    if (MDNS.begin("esp32")) {
+        Serial.println("MDNS responder started");
     }
 
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+    Serial.println();
+    Serial.println("Configuring access point...");
+    if (!WiFi.softAP("Octo-Tree Board", "12345678")) {
+        log_e("Soft AP creation failed.");
+        while (1);
+    }
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+    server.begin();
+
+    Serial.println("Server started");
 }
 
 void readResponse(WiFiClient& client) {
@@ -50,7 +52,16 @@ void readResponse(WiFiClient& client) {
     Serial.printf("\nClosing connection\n\n");
 }
 
-void loop() {
+void connectToWiFi(String ssid, String password) {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to the WiFi network");
+}
+
+void loop2() {
     WiFiClient client;
     String footer = String(" HTTP/1.1\r\n") + "Host: " + String(host) + "\r\n" + "Connection: close\r\n\r\n";
 
@@ -63,4 +74,58 @@ void loop() {
     readResponse(client);
 
     delay(10000);
+}
+
+
+void loop() {
+  NetworkClient client = server.accept();  // listen for incoming clients
+
+  if (client) {                     // if you get a client,
+    Serial.println("New Client.");  // print a message out the serial port
+    String currentLine = "";        // make a String to hold incoming data from the client
+    while (client.connected()) {    // loop while the client's connected
+      if (client.available()) {     // if there's bytes to read from the client,
+        char c = client.read();     // read a byte, then
+        Serial.write(c);            // print it out the serial monitor
+        if (c == '\n') {            // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {  // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          Serial.println("TURNING ON LIGHT");
+          digitalWrite(LED_BUILTIN, HIGH);  // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          Serial.println("TURNING OFF LIGHT");
+          digitalWrite(LED_BUILTIN, LOW);  // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
 }
