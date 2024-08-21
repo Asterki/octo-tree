a2a726b918d39714ce28e2ba5a4b773836f59e69
asterki <asterki.dev@proton.me>
2024-08-20 19:38:45 -0600

    In-Progress: Azure storage service

--- server/src/services/azure/storage.ts
+++ server/src/services/azure/storage.ts
@@ -1,38 +1,59 @@
-import { BlobServiceClient } from "@azure/storage-blob";
+import { BlobServiceClient, AnonymousCredential } from '@azure/storage-blob'
 
 class AzureStorageService {
-    private blobServiceClient: BlobServiceClient;
+	private blobServiceClient: BlobServiceClient
+	private connectionStr: string
 
-    constructor(connectionString: string) {
-        this.blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
-    }
+	private static instance: AzureStorageService | null = null
 
-    async createContainer(containerName: string): Promise<void> {
-        const containerClient = this.blobServiceClient.getContainerClient(containerName);
-        await containerClient.create();
-    }
+	constructor() {
+		this.connectionStr = process.env
+			.AZURE_STORAGE_CONNECTION_STRING as string
+		console.log(this.connectionStr)
+		this.blobServiceClient = new BlobServiceClient(
+			this.connectionStr,
+			new AnonymousCredential()
+		)
 
-    async uploadFile(containerName: string, fileName: string, fileContent: Buffer): Promise<void> {
-        const containerClient = this.blobServiceClient.getContainerClient(containerName);
-        const blockBlobClient = containerClient.getBlockBlobClient(fileName);
-        await blockBlobClient.uploadData(fileContent);
-    }
+		this.printContainers()
+	}
 
-    async downloadFile(containerName: string, fileName: string): Promise<Buffer> {
-        const containerClient = this.blobServiceClient.getContainerClient(containerName);
-        const blockBlobClient = containerClient.getBlockBlobClient(fileName);
-        const downloadResponse = await blockBlobClient.download();
-        const buffer = await downloadResponse.blobBody?.getBuffer();
-        if (!buffer) {
-            throw new Error("Failed to download file");
-        }
-        return buffer;
-    }
+	public static getInstance() {
+		if (!AzureStorageService.instance)
+			AzureStorageService.instance = new AzureStorageService()
+		return AzureStorageService.instance
+	}
 
-    async deleteContainer(containerName: string): Promise<void> {
-        const containerClient = this.blobServiceClient.getContainerClient(containerName);
-        await containerClient.delete();
-    }
+	private async printContainers() {
+		let i = 1
+		let iter = this.blobServiceClient.listContainers()
+		for await (const container of iter) {
+			console.log(`Container ${i++}: ${container.name}`)
+		}
+	}
+
+	async createContainer(containerName: string): Promise<void> {
+		const containerClient =
+			this.blobServiceClient.getContainerClient(containerName)
+		await containerClient.create()
+	}
+
+	async uploadFile(
+		containerName: string,
+		fileName: string,
+		fileContent: Buffer
+	): Promise<void> {
+		const containerClient =
+			this.blobServiceClient.getContainerClient(containerName)
+		const blockBlobClient = containerClient.getBlockBlobClient(fileName)
+		await blockBlobClient.uploadData(fileContent)
+	}
+
+	async deleteContainer(containerName: string): Promise<void> {
+		const containerClient =
+			this.blobServiceClient.getContainerClient(containerName)
+		await containerClient.delete()
+	}
 }
 
-export default AzureStorageService;
+export default AzureStorageService
