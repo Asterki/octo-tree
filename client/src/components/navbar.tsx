1aa1f6e683e09f5950cc04fb725ab62b623e4c50
asterki <asterki.dev@proton.me>
2024-08-25 13:08:47 -0600

    feat: Improve home page banner and content, update navbar styling and links, add privacy policy page, and refactor unused Azure environment variables

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -2,17 +2,17 @@ import { Link } from 'react-router-dom'
 
 const NavbarComponent = () => {
 	return (
-		<div className="absolute top-0 w-full md:flex-row flex-col  bg-gray-700 text-white flex items-center justify-between md:px-16 px-4 py-4 md:h-16 h-32">
+		<div className="absolute top-0 w-full md:flex-row flex-col bg-gray-800 text-white flex items-center justify-between md:px-16 px-4 py-4 md:h-16 h-32 z-30">
 			<div className="text-2xl font-bold">
-				<h1>Octo-Tree</h1>
+				<Link to="/">Octo-Tree</Link>
 			</div>
 
 			<div className="flex items-center justify-center md:gap-2">
 				<div className="group relative">
-					<h1 className="cursor-pointer bg-emerald-600 rounded-md p-2 md:scale-100 scale-90">
+					<h1 className="cursor-pointer bg-emerald-600 text-white rounded-md p-2 md:scale-100 scale-90">
 						About the App
 					</h1>
-					<div className="flex-col gap-2 top-16 shadow-md bg-white text-gray-800 max-h-0 group-hover:max-h-72 overflow-y-hidden transition-all ease-in-out duration-500 flex absolute w-full">
+					<div className="flex-col gap-2 top-12 shadow-md bg-white text-gray-800 max-h-0 group-hover:max-h-72 overflow-y-hidden transition-all ease-in-out duration-500 flex absolute w-full">
 						<Link
 							to={'https://asterki.tech'}
 							target="_blank"
@@ -36,10 +36,10 @@ const NavbarComponent = () => {
 					</div>
 				</div>
 				<div className="group relative">
-					<h1 className="cursor-pointer bg-emerald-600 rounded-md p-2 md:scale-100 scale-90">
+					<h1 className="cursor-pointer bg-emerald-600 text-white rounded-md p-2 md:scale-100 scale-90">
 						Get an Octo-Tree
 					</h1>
-					<div className="flex-col gap-2 top-16 shadow-md bg-white text-gray-800 max-h-0 group-hover:max-h-72 overflow-y-hidden transition-all ease-in-out duration-500 flex absolute  w-full">
+					<div className="flex-col gap-2 top-12 shadow-md bg-white text-gray-800 max-h-0 group-hover:max-h-72 overflow-y-hidden transition-all ease-in-out duration-500 flex absolute  w-full">
 						<nav className="hover:bg-gray-200 p-2 cursor-pointer w-full">
 							Still not public
 						</nav>
@@ -49,10 +49,10 @@ const NavbarComponent = () => {
 					</div>
 				</div>
 				<div className="group relative">
-					<h1 className="cursor-pointer bg-emerald-600 rounded-md p-2 md:scale-100 scale-90">
+					<h1 className="cursor-pointer bg-emerald-600 text-white rounded-md p-2 md:scale-100 scale-90">
 						Account Access
 					</h1>
-					<div className="flex-col gap-2 top-16 shadow-md bg-white text-gray-800 max-h-0 group-hover:max-h-72 overflow-y-hidden transition-all ease-in-out duration-500 flex absolute  w-full">
+					<div className="flex-col gap-2 top-12 shadow-md bg-white text-gray-800 max-h-0 group-hover:max-h-72 overflow-y-hidden transition-all ease-in-out duration-500 flex absolute  w-full">
 						<Link
 							className="hover:bg-gray-200 p-2 cursor-pointer w-full"
 							to="/login"
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -13,21 +13,50 @@ const HomePage = () => {
 	const user = useAppSelector((state) => state.page.user)
 	const dispatch = useAppDispatch()
 
+	// Check if the user is already logged in
+	React.useEffect(() => {
+		;(async () => {
+			if (user) {
+				navigate('/dashboard')
+			} else {
+				// Get the user's data
+				try {
+					const response = await axios({
+						url: `${import.meta.env.VITE_API_URL}/api/accounts/me`,
+						method: 'get',
+						withCredentials: true,
+					})
+
+					dispatch(setUser(response.data))
+				} catch (error) {
+					console.error(error)
+				}
+			}
+		})()
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
 	return (
-		<div className="bg-neutral-100 min-h-screen text-neutral-600">
+		<div className="bg-neutral-100 md:h-[calc(100vh-4rem)] h-[calc(100vh-8rem)] text-neutral-600">
 			<NavbarComponent />
 
-			<main className="flex flex-col items-start justify-start h-full md:mt-16 mt-32">
+			<main className="flex flex-col items-start justify-start md:h-[calc(100vh-4rem)] h-[calc(100vh-8rem)] md:mt-16 mt-32">
 				<section className="w-full">
-					<div className=" text-gray-800 relative flex items-center justify-center h-[calc(100vh)] bg-[url('/wallpaper.jpg')] backdrop-blur-sm">
-					
+					<div className=" text-gray-800 relative flex items-center justify-center md:h-[calc(100vh-4rem)] h-[calc(100vh-8rem)] bg-gray-800">
+						<video
+							src="/bg.mp4"
+							autoPlay
+							muted
+							loop
+							className="absolute top-0 left-0 w-full h-full md:object-fill object-cover"
+						></video>
 
-						<div className="absolute text-white px-10 w-full">
+						<div className="text-center md:text-left absolute text-white px-10 md:w-full">
 							<h1 className="text-5xl font-bold">
 								The future of modular IoT starts here.
 							</h1>
 
-							<p className="w-5/12 px-4">
+							<p className="md:w-5/12">
 								Octo-Tree is a modular IoT platform that allows
 								you to quickly and easily configure and deploy
 								IoT solutions, all from within a web browser.
@@ -35,10 +64,10 @@ const HomePage = () => {
 
 							<p>
 								<button
-									onClick={() => navigate('/register')}
-									className="bg-emerald-600 text-white px-4 py-2 rounded-md mt-4"
+									onClick={() => navigate('/dashboard')}
+									className="bg-emerald-600 text-white px-4 py-2 rounded-md mt-4 shadow-md"
 								>
-									Get started
+									{user ? 'Dashboard' : 'Get Started'}
 								</button>
 							</p>
 						</div>
