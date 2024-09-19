e608af4336baeadf843668363bd18bbf38a9fe01
asterki <asterki.dev@proton.me>
2024-09-18 20:47:17 -0600

    Refactor: Code optimization

--- server/src/controllers/routines.ts
+++ server/src/controllers/routines.ts
@@ -1,176 +1,160 @@
 import { PrismaClient } from '@prisma/client'
 
-// Basically what this does is that it watched the database every 5 seconds, watching for changes
-// in the routines table. If a routine with a "nextExecutionInterval" with a time less than the current
-// time, it will add the routine to the triggeredActions table, which will be executed by the board
-// Once that's updated, the routine will be updated with a new "nextExecutionInterval" based on the
-// "checkInterval" field
 class RoutineController {
 	private static instance: RoutineController | null = null
+	private prisma = new PrismaClient()
+	private intervalId: NodeJS.Timeout | undefined
 
 	public static getInstance() {
-		if (!RoutineController.instance)
+		if (!RoutineController.instance) {
 			RoutineController.instance = new RoutineController()
+		}
 		return RoutineController.instance
 	}
 
 	public async loadToServer() {
-		const prisma = new PrismaClient()
-
-		setInterval(async () => {
-			const routines = await prisma.routine.findMany({
-				where: {
-					automatedExecution: {
-						// This is datetime object,
-						nextExecutionInterval: {
-							lte: new Date(Date.now()),
-						},
-					},
-				},
-				include: {
-					actions: {
-						select: {
-							notify: true,
-							water: true,
-							rotatePanel: true,
-						},
-					},
-					automatedExecution: {
-						select: {
-							conditions: {
-								select: {
-									humidityBelow: true,
-									humidityExceeds: true,
-									temperatureBelow: true,
-									temperatureExceeds: true,
-								},
-							},
-							nextExecutionInterval: true,
-							checkInterval: true,
-						},
-					},
-				},
-			})
-
-			if (!routines) return
-
-			for (const routine of routines) {
-				let board = await prisma.board.findFirst({
+		this.intervalId = setInterval(async () => {
+			try {
+				const routines = await this.prisma.routine.findMany({
 					where: {
-						id: routine.boardId!,
+						automatedExecution: {
+							// This is datetime object,
+							nextExecutionInterval: {
+								lte: new Date(Date.now()),
+							},
+						},
+					},
+					include: {
+						actions: {
+							select: {
+								notify: true,
+								water: true,
+								rotatePanel: true,
+							},
+						},
+						automatedExecution: {
+							select: {
+								conditions: {
+									select: {
+										humidityBelow: true,
+										humidityExceeds: true,
+										temperatureBelow: true,
+										temperatureExceeds: true,
+									},
+								},
+								nextExecutionInterval: true,
+								checkInterval: true,
+							},
+						},
 					},
 				})
-				if (!board) return
-				let sensorData = JSON.parse(board.sensorData)
 
-				const executeRoutine = async (routine: any) => {
-					if (routine.actions.notify) {
-						// Send a notification
-					}
+				if (!routines) return
 
-					if (routine.actions.water) {
-						// First check if there are any pending water action
-						let pendingWaterAction =
-							await prisma.triggeredActions.findFirst({
-								where: {
-									actionType: 'water',
-									boardId: routine.boardId,
-								},
-							})
+				for (const routine of routines) {
+					const board = await this.prisma.board.findFirst({
+						where: { id: routine.boardId! },
+					})
+					if (!board) return
+					const sensorData = JSON.parse(board.sensorData)
 
-						if (!pendingWaterAction) {
-							await prisma.triggeredActions.create({
-								data: {
-									actionType: 'water',
-									boardId: routine.boardId,
-									actionValue: routine.actions.water.amount,
-								},
-							})
+					if (routine.execution === 'automated') {
+						// Check conditions
+						if (
+							this.checkCondition(
+								routine!.automatedExecution!.conditions!
+									.humidityBelow,
+								sensorData.humidity
+							) ||
+							this.checkCondition(
+								routine!.automatedExecution!.conditions!
+									.humidityExceeds,
+								sensorData.humidity
+							) ||
+							this.checkCondition(
+								routine!.automatedExecution!.conditions!
+									.temperatureBelow,
+								sensorData.temperature
+							) ||
+							this.checkCondition(
+								routine!.automatedExecution!.conditions!
+									.temperatureExceeds,
+								sensorData.temperature
+							)
+						) {
+							await this.executeRoutine(routine)
 						}
-					}
 
-					if (routine.actions.rotatePanel) {
-						// First check if there are any pending rotatePanel action
-						let pendingRotatePanelAction =
-							await prisma.triggeredActions.findFirst({
-								where: {
-									actionType: 'rotatePanel',
-									boardId: routine.boardId,
-								},
-							})
-
-						if (!pendingRotatePanelAction) {
-							await prisma.triggeredActions.create({
-								data: {
-									actionType: 'rotatePanel',
-									boardId: routine.boardId,
-									actionValue: 0.0,
-								},
-							})
-						}
-					}
-				}
-
-				if (routine.execution == 'automated') {
-					// Check if the routine is automated
-					// Check the conditions for the routine
-
-					if (
-						routine.automatedExecution?.conditions?.humidityBelow
-							.active &&
-						routine.automatedExecution.conditions.humidityBelow
-							.value >= sensorData.humidity
-					) {
-						executeRoutine(routine)
-					}
-
-					if (
-						routine.automatedExecution?.conditions?.humidityExceeds
-							.active &&
-						routine.automatedExecution.conditions.humidityExceeds
-							.value <= sensorData.humidity
-					) {
-						executeRoutine(routine)
-					}
-
-					if (
-						routine.automatedExecution?.conditions?.temperatureBelow
-							.active &&
-						routine.automatedExecution.conditions.temperatureBelow
-							.value >= sensorData.temperature
-					) {
-						executeRoutine(routine)
-					}
-
-					if (
-						routine.automatedExecution?.conditions
-							?.temperatureExceeds.active &&
-						routine.automatedExecution.conditions.temperatureExceeds
-							.value <= sensorData.temperature
-					) {
-						executeRoutine(routine)
-					}
-
-					// Update the routine
-					await prisma.routine.update({
-						where: {
-							id: routine.id,
-						},
-						data: {
-							automatedExecution: {
-								update: {
-									nextExecutionInterval: new Date(
-										Date.now() +
-											routine.automatedExecution!
-												.checkInterval
-									),
+						// Update the routine
+						await this.prisma.routine.update({
+							where: { id: routine.id },
+							data: {
+								automatedExecution: {
+									update: {
+										nextExecutionInterval: new Date(
+											Date.now() +
+												routine.automatedExecution!
+													.checkInterval
+										),
+									},
 								},
 							},
-						},
-					})
+						})
+					}
 				}
+			} catch (error) {
+				console.error('Error in loadToServer:', error)
 			}
 		}, 5000)
+	}
+
+	private async executeRoutine(routine: any) {
+		if (routine.actions.notify) {
+			// Send a notification
+		}
+
+		if (routine.actions.water) {
+			// First check if there are any pending water action
+			this.createIfNotExists(
+				'water',
+				routine.boardId,
+				routine.actions.water.amount
+			)
+		}
+
+		if (routine.actions.rotatePanel) {
+			// First check if there are any pending rotatePanel action
+			this.createIfNotExists(
+				'rotatePanel',
+				routine.boardId,
+				routine.actions.rotatePanel.amount
+			)
+		}
+	}
+
+	private async createIfNotExists(
+		actionType: string,
+		boardId: string,
+		actionValue: number
+	) {
+		const pendingAction = await this.prisma.triggeredActions.findFirst({
+			where: { actionType, boardId },
+		})
+
+		if (!pendingAction) {
+			await this.prisma.triggeredActions.create({
+				data: { actionType, boardId, actionValue },
+			})
+		}
+	}
+
+	private checkCondition(condition: any, sensorValue: number): boolean {
+		return condition.active && condition.value >= sensorValue
+	}
+
+	public async cleanup() {
+		clearInterval(this.intervalId)
+		await this.prisma.$disconnect()
 	}
 }
 
